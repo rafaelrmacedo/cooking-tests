5425d78a394c53a1792b3da6fb69bbfaa1bc5b0b
Rafael Macedo <rafael.macedo@jala.university>
2026-03-05 15:47:21 -0300

    add: folders for backend endpoints

---
+++ tests/bookings/test.spec.ts
@@ -0,0 +1,7 @@
+import test from "@playwright/test";
+import { RequestLogger } from "../../helpers/log/request.logger";
+const logger = new RequestLogger();
+
+test("Test", async ({ request }) => {
+
+});
