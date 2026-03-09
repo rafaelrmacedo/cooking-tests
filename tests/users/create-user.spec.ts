3c0d5b851fcc57eed01589ddc22ab27c3699d805
Rafael Macedo <rafaelrodriguesmacedo2004@gmail.com>
2026-03-09 08:50:46 -0300

    add: failure create user test (email)

--- tests/users/create-user.spec.ts
+++ tests/users/create-user.spec.ts
@@ -4,7 +4,7 @@ import { HttpHandler } from "../../helpers/handler/http-handler";
 import { RequestLogger } from "../../helpers/log/request.logger";
 const logger = new RequestLogger();
 
-test("Create user", async ({ request }) => {
+test("Create user - Success", async ({ request }) => {
   const http = new HttpHandler(request, logger);
   const payload = {
     name: faker.person.firstName(),
@@ -15,4 +15,18 @@ test("Create user", async ({ request }) => {
   const response = await http.onUsersApi().createNewUser(payload);
 
   expect(response.response.status()).toBe(201);
+});
+
+test.only("Create user - Failure (Invalid email)", async ({ request }) => {
+  const http = new HttpHandler(request, logger);
+  const payload = {
+    name: "teste",
+    email: faker.number.int({ min: 1, max: 9999 }).toString(),
+    password: "teste",
+  };
+
+  const response = await http.onUsersApi().createNewUser(payload);
+
+  expect(response.response.status()).toBe(400);
+  expect(response.jsonResponse.message).toContain("email must be an email");
 });
