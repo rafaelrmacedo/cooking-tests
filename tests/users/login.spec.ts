6d8ef0eb0592ad885345b2446dd5d1d795bc7055
Rafael Macedo <rafaelrodriguesmacedo2004@gmail.com>
2026-03-11 19:35:38 -0300

    feat: invalid credentials login test

--- tests/users/login.spec.ts
+++ tests/users/login.spec.ts
@@ -19,3 +19,25 @@ test("Login", async ({ request }) => {
 
   expect(response.response.status()).toBe(201);
 });
+
+test("Login - Failure (Invalid password)", async ({ request }) => {
+  const http = new HttpHandler(request, logger);
+
+  const newUser = {
+    name: faker.person.firstName(),
+    email: faker.number.int({ min: 1, max: 9999 }) + faker.internet.email(),
+    password: "teste123123",
+  };
+
+  await http.onUsersApi().createNewUser(newUser);
+
+  const invalidPasswordUser = {
+    email: newUser.email,
+    password: "invalid password",
+  };
+
+  const response = await http.onUsersApi().login(invalidPasswordUser);
+
+  expect(response.response.status()).toBe(401);
+  expect(response.jsonResponse.message).toContain("Invalid credentials");
+});
