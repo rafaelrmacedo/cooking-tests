607f9bd96fd0547b5d11d4acd63bb8b7d0de0fe0
Rafael Macedo <rafaelrodriguesmacedo2004@gmail.com>
2026-08-03 15:50:43 -0300

    feat(lib): add support as a library

---
+++ index.ts
@@ -0,0 +1,5 @@
+export { test } from './helpers/fixtures/test.fixture';
+export { HttpHandler } from './helpers/handler/http-handler';
+export { RequestLogger } from './helpers/log/request.logger';
+export { ContractValidator } from './helpers/contract/contract.validator';
+export type { TestFixtures } from './helpers/types/test-fixtures.type';
